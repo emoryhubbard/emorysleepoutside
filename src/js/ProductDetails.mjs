d331c7216fd44bc3147c1cd5124906df67d72e5e
emoryhubbardbyui <hub21003@byui.edu>
2023-01-20 17:07:05 -0500

    eliminated flicker and placeholder img

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -28,14 +28,33 @@ export default class ProductDetails {
     }
     renderProductDetails() {
         let p = this.product;
+        qs(".product-detail").innerHTML =
+        `<h3 class="brand">${p.Brand.Name}</h3>
 
-        qs(".brand").innerHTML = p.Brand.Name;
+        <h2 class="divider">${p.NameWithoutBrand}</h2>
+
+        <img
+          class="divider product-image"
+          src="${p.Image}"
+          alt="${p.NameWithoutBrand}"
+        />
+
+        <p class="product-card__price">$${p.ListPrice}</p>
+
+        <p class="product__color color-name">${p.Colors[0].ColorName}</p>
+
+        <p class="product__description">${p.DescriptionHtmlSimple}</p>
+
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${p.Id}">Add to Cart</button>
+        </div>`;
+        /*qs(".brand").innerHTML = p.Brand.Name;
         qs(".name-without-brand").innerHTML = p.NameWithoutBrand;
         qs(".product-image").src = p.Image;
         qs(".list-price").innerHTML = "$" + p.ListPrice;
         let colorObject = p.Colors[0];
         qs(".color-name").innerHTML = colorObject.ColorName;
         qs(".description-html-simple").innerHTML = p.DescriptionHtmlSimple;    
-        qs("#addToCart").setAttribute("data-id", p.Id);
+        qs("#addToCart").setAttribute("data-id", p.Id);*/
     }
 }
